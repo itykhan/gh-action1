08198fb9c15ad09f365fa6a5492da069756a3434
Irina <itykhan@gmail.com>
2023-05-12 14:49:27 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-set -e
-
-sudo ectool --debug 1 --server $CDRO_HOST login $CDRO_USER $CDRO_PASSWORD
-sudo ectool runPipeline it-test gha-test --actualParameter repo=$CDRO_REPO
+ectool --server $CDRO_HOST login $CDRO_USER $CDRO_PASSWORD
+ectool runPipeline it-test gha-test --actualParameter repo=$CDRO_REPO
