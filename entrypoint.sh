684702bce1908d15016c02c12df538091751ee1a
Irina <itykhan@gmail.com>
2023-05-12 15:37:13 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,7 +3,9 @@
 export COMMANDER_SESSIONFILE=/github/workspace/.ecsession
 
 echo $COMMANDER_SESSIONFILE
-echo 'test'>/github/workspace/test.txt
+echo 'test'>/tmp/test.txt
+pwd
+echo $USER
 
 ectool --server $CDRO_HOST login $CDRO_USER $CDRO_PASSWORD
 ectool runPipeline it-test gha-test --actualParameter repo=$CDRO_REPO
