ff4200cb13dfbbe900658019f2acd1e2aaf8d56d
Irina <itykhan@gmail.com>
2023-05-12 14:07:58 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
 
-ectool --debug 1 --server $CDRO_HOST login $CDRO_USER $CDRO_PASSWORD
-ectool runPipeline it-test gha-test --actualParameter repo=$GITHUB_ACTION_REPOSITORY
+ectool --debug 1 --server "${CDRO_HOST}" login "${CDRO_USER}" $CDRO_PASSWORD
+ectool runPipeline it-test gha-test --actualParameter repo="${CDRO_REPO}"
+"
